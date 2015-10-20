fa86f8c5a34fbd0891944a14ea3a699aaaa21927
Sushil Parajuli <jambilateralus@gmail.com>
2015-10-20 11:26:53 +0545

    calendar spelling correction :-D

--- app/src/main/java/com/project/myschedule/MyActivity.java
+++ app/src/main/java/com/project/myschedule/MyActivity.java
@@ -19,7 +19,6 @@ public class MyActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
-
         return true;
     }
 
