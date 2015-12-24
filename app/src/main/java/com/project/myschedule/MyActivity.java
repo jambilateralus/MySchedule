84a482e2126c09be71e79f75ff0e72c2213a1b66
sushil <jambilateralus@gmail.com>
2015-12-24 14:18:55 +0545

    cardView

--- app/src/main/java/com/project/myschedule/MyActivity.java
+++ app/src/main/java/com/project/myschedule/MyActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,17 +17,27 @@ import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
 
 
 public class MyActivity extends Activity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_card_view);
 
 
-        ListView lv = (ListView) findViewById(R.id.listView);
+
+
+
+        //ListView lv = (ListView) findViewById(R.id.listView);
         DataBase db = new DataBase(this);
         db.open();
         final String[] scheduleTitle = new String[db.getScheduleCount()];
@@ -32,32 +45,41 @@ public class MyActivity extends Activity {
         String[] scheduleFrom = new String[db.getScheduleCount()];
 
 
+        ArrayList<String> list = new ArrayList<String>();
         //Load database contents to arrays
         for (int i = 0; i < db.getScheduleCount(); i++) {
             scheduleTitle[i] = db.getScheduleTitle(i);
             scheduleTo[i] = db.getScheduleToDate(i);
             scheduleFrom[i] = db.getScheduleFromDate(i);
+            list.add(db.getScheduleTitle(i));
         }
         db.close();
 
 
-        //set custom adapter to list view
-        ScheduleAdapter adpt = new ScheduleAdapter(this, scheduleTitle, scheduleTo, scheduleFrom);
-        lv.setAdapter(adpt);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyRecyclerViewAdapter(getDataSet());
+        mRecyclerView.setAdapter(mAdapter);
 
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //set custom adapter to list view
+        //ScheduleAdapter adpt = new ScheduleAdapter(this, scheduleTitle, scheduleTo, scheduleFrom);
+        //lv.setAdapter(adpt);
+
+        //lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+          //  @Override
+            //public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //Toast.makeText(getBaseContext(), "You have selected item : " + scheduleTitle[position], Toast.LENGTH_SHORT).show();
 
-                Intent taskList = new Intent(view.getContext(),TaskListActivity.class);
+              //  Intent taskList = new Intent(view.getContext(),TaskListActivity.class);
 
-                taskList.putExtra("hello",scheduleTitle[position]);
+                //taskList.putExtra("hello",scheduleTitle[position]);
 
-                startActivity(taskList);
+                //startActivity(taskList);
 
-            }
-        });
+            //}
+        //});
 
 
     }
@@ -94,5 +116,26 @@ public class MyActivity extends Activity {
 
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ((MyRecyclerViewAdapter) mAdapter).setOnItemClickListener(new MyRecyclerViewAdapter
+                .MyClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                Log.i(LOG_TAG, " Clicked on Item " + position);
+            }
+        });
+    }
+
+    private ArrayList<DataObject> getDataSet() {
+        ArrayList results = new ArrayList<DataObject>();
+        for (int index = 0; index < 20; index++) {
+            DataObject obj = new DataObject("Some Primary Text " + index,
+                    "Secondary " + index);
+            results.add(index, obj);
+        }
+        return results;
+    }
 
 }
